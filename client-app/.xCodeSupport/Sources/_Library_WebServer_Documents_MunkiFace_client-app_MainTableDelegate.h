758f5f8df8a99023dd160d5fa61aefdade47b632
Joe Wollard <joe.wollard@gmail.com>
2012-02-08 07:03:12 -0500

    cleaning

--- client-app/.xCodeSupport/Sources/_Library_WebServer_Documents_MunkiFace_client-app_MainTableDelegate.h
+++
@@ -1,8 +0,0 @@
-
-@interface MainTableDelegate : NSObject
-{
-    IBOutlet NSTextField* _metaLabel;
-    IBOutlet NSTextField* _metaDescription;
-}
-
-@end
--- client-app/.xCodeSupport/Sources/_Library_WebServer_Documents_MunkiFace_client-app_Pkg.h
+++
@@ -1,7 +0,0 @@
-
-@interface Pkg : NSObject
-{
-
-}
-
-@end
