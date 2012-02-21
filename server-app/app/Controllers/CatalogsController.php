14bfe7d86c5ef176899841af763751d3d7439bbe
Joe Wollard <joe.wollard@gmail.com>
2012-02-21 14:56:22 -0500

    Removed the CatalogsModel.php file since PkgsInfoModel.php does everything that's needed from the Catalog controller at the moment.

--- server-app/app/Controllers/CatalogsController.php
+++ server-app/app/Controllers/CatalogsController.php
@@ -3,11 +3,30 @@
 
 class CatalogsController extends RTObject
 {
+	protected static $_catalogs;
+
+
 	public function init()
 	{
 		parent::init();
-		$model = CatalogsModel::catalogs();
-		echo $model;
+		$model = PkgsInfoModel::alloc()->init()->plists();
+
+		self::$_catalogs = RTMutableArray::anArray();
+
+		foreach($model as $plist)
+		{
+			$catalogs = $plist["catalogs"];
+			foreach($catalogs as $catalog)
+			{
+				if (!self::$_catalogs->containsObject($catalog))
+				{
+					self::$_catalogs->addObject($catalog);
+				}
+			}
+		}
+
+		echo json_encode(self::$_catalogs->phpArray());
+
 		return $this;
 	}
 }
