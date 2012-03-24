e9d8f4e68ef4bd2f535b9d1dcc68019d250510ec
Joe Wollard <joe.wollard@gmail.com>
2012-03-24 12:44:59 -0400

    Adding in a few MFError type constants that contain error codes.

--- server-app/app/bootstrap.php
+++ server-app/app/bootstrap.php
@@ -4,6 +4,12 @@ ini_set('display_errors', 0);
 $appDir = dirname(__FILE__) . "/";
 
 
+
+define("MFParseError", 1);
+define("MFInvalidTargetForAction", 2);
+
+
+
 require_once($appDir . "Models/JSONException.php");
 require_once($appDir . "RTFoundation/Frameworks/Foundation/Foundation.php");
 require_once($appDir . "Settings.php");
